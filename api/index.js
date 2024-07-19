b1002e3ed38773cf5637c50acd99101cffc367b3
Priyansh <priyanshpatel234@gmail.com>
2024-07-19 15:07:54 +0530

    add signup, signin functionality with jwt and bcryptjs

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "../routes/user-router.js";
+import commentRouter from "../routes/comment-router.js";
+import videoRouter from "../routes/video-router.js";
+import authRouter from "../routes/auth-router.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,7 +20,22 @@ mongoose
 
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/video", videoRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ controller/auth-controller.js
@@ -0,0 +1,50 @@
+import User from "../model/user-model.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+// ***********************************
+// SignUp Logic
+// ***********************************
+
+export const signUp = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const newUser = new User({ ...req.body, password: hashPassword });
+
+    await newUser.save();
+    res.status(200).json("User has been created!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+// SignInLogic
+// ***********************************
+
+export const signIn = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+
+    if (!isCorrect) {
+      return next(errorHandler(400, "Invalid Credential"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN);
+
+    const { password, ...rest } = user._doc;
+
+    res.cookie("access_token", token).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
