4ac9194a4b50b3061d5c56a44d042617fe8addd0
Priyansh <priyanshpatel234@gmail.com>
2024-07-19 12:38:30 +0530

    download react, and create server and connect to mongoDb

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((error) => {
+    console.log("Error From Database", error);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
