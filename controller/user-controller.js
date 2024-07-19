70513f6b012770bb262c9fea66e163b6a57abfb5
Priyansh <priyanshpatel234@gmail.com>
2024-07-19 15:33:28 +0530

    add update functionality with verifyToken

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,3 +1,21 @@
-export const test = (req, res) => {
-  res.json("user Successfully");
+import { errorHandler } from "../utils/error.js";
+import User from "../model/user-model.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id !== req.user.id) {
+    return next(errorHandler(403, "You are not allowed to update Data"));
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { test } from "../controller/user-controller.js";
+import { verifyToken } from "../utils/verifyToken.js";
+import { updateUser } from "../controller/user-controller.js";
 
 const router = express.Router();
 
-router.get("/", test);
+router.put("/update/:id", verifyToken, updateUser);
 
 export default router;
