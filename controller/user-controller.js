6084e7715d9fb0746dc63f63ef1ddb7bc4962322
Priyansh <priyanshpatel234@gmail.com>
2024-07-19 16:39:25 +0530

    add getUser, subscribe, unSubscribe functionality

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,9 +1,13 @@
 import { errorHandler } from "../utils/error.js";
 import User from "../model/user-model.js";
 
+// ***********************************
+// Update User functionality
+// ***********************************
+
 export const updateUser = async (req, res, next) => {
   if (req.params.id !== req.user.id) {
-    return next(errorHandler(403, "You are not allowed to update Data"));
+    return next(errorHandler(403, "You are not allowed to update User"));
   }
   try {
     const updatedUser = await User.findByIdAndUpdate(
@@ -15,6 +19,76 @@ export const updateUser = async (req, res, next) => {
     );
 
     res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+// Delete User functionality
+// ***********************************
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id !== req.user.id) {
+    return next(errorHandler(403, "You are not allowed to Delete User"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("User Account Delete Successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+// get User functionality
+// ***********************************
+
+export const getUser = async (req, res, next) => {
+  try {
+    const getUserData = await User.findById(req.params.id);
+    res.status(200).json(getUserData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+//  Subscribe Channel functionality
+// ***********************************
+
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $push: { subscribedUser: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+
+    res.status(200).json("Subscribe Successfully!!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// ***********************************
+//  unSubscribe Channel functionality
+// ***********************************
+
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $pull: { subscribedUser: req.params.id },
+    });
+
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+
+    res.status(200).json("Unsubscribe Successfully!!");
   } catch (error) {
     next(error);
   }
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { updateUser } from "../controller/user-controller.js";
+import {
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  updateUser,
+} from "../controller/user-controller.js";
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
+
+router.delete("/delete/:id", verifyToken, deleteUser);
+
+router.get("/getUser/:id", getUser);
+
+router.put("/subscribe/:id", verifyToken, subscribe);
+
+router.put("/unsubscribe/:id", verifyToken, unsubscribe);
 
 export default router;
